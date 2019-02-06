8f6ba9d10ca72591c13da1e41872d24a5a54cb61
Gayle J <gayle@netsparkle.com>
2019-02-06 08:20:02 -0500

    Create check-file-status.php

---
+++ check-file-status.php
@@ -0,0 +1,30 @@
+<?php
+//simple script to check the status of a file in a directory. Trigger it with a cron job or by vising this file in a web browser.
+$filename = 'file.xml';
+$filemtime = filemtime($filename);
+$time = 129600; //36 hours in seconds
+
+//if the file is missing
+if( ! file_exists($filename) ) {
+	echo "the file is missing";
+    $msg = "The file is missing. please check for a problem.";
+mail("gayle@netsparkle.com","ALERT: FILE IS MISSING",$msg);	
+}
+//if the file is not missing
+else{
+//if the file is older than 36 hours
+if( time() - $filemtime >= $time ) {
+	echo "the file is stale";
+    $modified = date ("F d Y H:i:s.", $filemtime);
+    $msg = "The file is stale. please check for a problem. It was last updated: {$modified}";
+mail("gayle@netsparkle.com","ALERT: FILE IS STALE",$msg);	
+	}
+else {
+//if the file is less than 36 hours old
+	echo "the file is ok";
+    $modified = date ("F d Y H:i:s.", $filemtime);
+    $msg = "The file last updated: {$modified}";
+mail("gayle@netsparkle.com","File Status",$msg);
+}
+}
+?> 
